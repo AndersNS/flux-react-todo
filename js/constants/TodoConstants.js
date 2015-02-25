cab64af7d73a48798071d529a9528adbe5276966
Anders N. Slinde <anders.slinde@gmail.com>
2015-02-25 22:54:20 +0100

    Added missing constant

--- js/constants/TodoConstants.js
+++ js/constants/TodoConstants.js
@@ -3,5 +3,6 @@ var keyMirror = require('react/lib/keyMirror');
 // Define action constants
 module.exports = keyMirror({
   TODO_ITEM_ADD: null,
-  TODO_ITEM_REMOVE: null
+  TODO_ITEM_REMOVE: null,
+  TODO_ITEM_DONE: null
 });
